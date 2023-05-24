307534fbe6f18be96219bc6fee4c4cc5fef7cc6d
Edgar Cisneros <cisns.edg@gmail.com>
2023-05-23 22:39:05 -0600

    Base project

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Edgar Cisneros on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
